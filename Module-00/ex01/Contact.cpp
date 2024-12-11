8dbbf03409e64e66eb29306a36eb499d6e5b04b8
M-elhamdaoui <melhamdaoui811@gmail.com>
2024-12-11 12:40:06 +0100

    module-00/ex01 add setters to Contact class

--- Module-00/ex01/Contact.cpp
+++ Module-00/ex01/Contact.cpp
@@ -6,6 +6,36 @@
 /*   By: mel-hamd <mel-hamd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 11:11:06 by mel-hamd          #+#    #+#             */
-/*   Updated: 2024/12/11 11:11:08 by mel-hamd         ###   ########.fr       */
+/*   Updated: 2024/12/11 12:37:58 by mel-hamd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Contact.hpp"
+#include <iostream>
+#include <iomanip>
+
+
+void Contact::setFirstName(std::string firstName)
+{
+	this->firsrName = firstName;
+}
+
+void Contact::setLastName(std::string lastName)
+{
+	this->lastName = lastName;	
+}
+
+void Contact::setNickname(std::string nickname)
+{
+	this->nickname = nickname;
+}
+
+void Contact::setPhoneNumber(std::string phoneNumber)
+{
+	this->phoneNumber = phoneNumber;
+}
+
+void Contact::setDarkestSecret(std::string darkestSecret)
+{
+	this->darkestSecret = darkestSecret;
+}
