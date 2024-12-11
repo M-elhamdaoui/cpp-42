821dcfc17fd47f8bfd40c3c4708a76f95bb0d419
Mohammed EL Hamdaoui <melhamdaoui811@gmail.com>
2024-12-11 11:54:00 +0100

    module-00/ex01 : create the class and define private attributes and public methods

--- Module-00/ex01/Contact.hpp
+++ Module-00/ex01/Contact.hpp
@@ -6,6 +6,32 @@
 /*   By: mel-hamd <mel-hamd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 11:11:09 by mel-hamd          #+#    #+#             */
-/*   Updated: 2024/12/11 11:11:11 by mel-hamd         ###   ########.fr       */
+/*   Updated: 2024/12/11 11:52:45 by mel-hamd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+#include <string>
+
+class Contact
+{
+private:
+	std::string firsrName;
+	std::string lastName;
+	std::string nickname;
+	std::string phoneNumber;
+	std::string darkestSecret;
+
+public:
+	void setFirstName(std::string firstName);
+	void setLastName(std::string lastName);
+	void setNickname(std::string nickname);
+	void setPhoneNumber(std::string PhoneNumber);
+	void setDarkestSecret(std::string DarkesetSecret);
+	void shortDisplay();
+	void detailsDesplay();
+};
+
+#endif
