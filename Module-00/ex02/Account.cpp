a313eca0cdf4e3591755ab4edf74f79a9717f28d
M-elhamdaoui <melhamdaoui811@gmail.com>
2024-12-13 15:01:03 +0100

    module-00/ex02 : start emplementing the contractor

--- Module-00/ex02/Account.cpp
+++ Module-00/ex02/Account.cpp
@@ -6,7 +6,29 @@
 /*   By: mel-hamd <mel-hamd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 12:32:08 by mel-hamd          #+#    #+#             */
-/*   Updated: 2024/12/13 12:32:09 by mel-hamd         ###   ########.fr       */
+/*   Updated: 2024/12/13 14:56:55 by mel-hamd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Account.hpp"
+#include <ctime>
+
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+void Account::_displayTimestamp()
+{
+	time_t now = time(NULL);
+}
+
+Account::Account( int initial_deposit  )
+{
+	this->_amount = initial_deposit;
+	this->_accountIndex = Account::_nbAccounts;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+	Account::_nbAccounts += 1;
+	Account::_totalAmount += initial_deposit;
+}
