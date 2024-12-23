37247f3615b9a6be251a6c6d060e34f1232dc229
M-elhamdaoui <melhamdaoui811@gmail.com>
2024-12-23 12:17:21 +0100

    Module-01/ex05 : init project and header file

---
+++ Module-01/ex05/Harl.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-hamd <mel-hamd@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/23 12:07:28 by mel-hamd          #+#    #+#             */
+/*   Updated: 2024/12/23 12:16:26 by mel-hamd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_CPP
+#define HARL_CPP
+
+# include <iostream>
+
+class Harl {
+	private :
+		void debug(void);
+		void info(void);
+		void warning(void);
+		void error(void);
+	public :
+		void complain(std::string level );
+};
+
+
+#endif
