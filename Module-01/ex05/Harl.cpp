04b97c8932dc1a6d0658b97d775018ad19564da9
M-elhamdaoui <melhamdaoui811@gmail.com>
2024-12-23 12:49:40 +0100

    Module-00/ex01 : implement the Harl.cpp

--- Module-01/ex05/Harl.cpp
+++ Module-01/ex05/Harl.cpp
@@ -6,7 +6,40 @@
 /*   By: mel-hamd <mel-hamd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 12:07:37 by mel-hamd          #+#    #+#             */
-/*   Updated: 2024/12/23 12:07:38 by mel-hamd         ###   ########.fr       */
+/*   Updated: 2024/12/23 12:48:50 by mel-hamd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+
+void Harl::debug(void){
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info(void){
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void Harl::warning(void){
+	std::cout << " think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month" << std::endl;
+}
+
+void Harl::error(void){
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+
+void Harl::complain(std::string level){
+	std::string names[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	void (Harl::*funs[])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	int i = 0;
+	while (i < 4)
+	{
+		if (names[i] == level)
+		{
+			(this->*funs[i])();
+			return ;
+		}
+		i++;
+	}
+}
