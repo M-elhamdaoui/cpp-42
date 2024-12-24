c95991d93003a38bcfdc22c009e7ed447d997e24
M-elhamdaoui <melhamdaoui811@gmail.com>
2024-12-24 14:11:53 +0100

    Module-00/ex04 : fix standard

--- Module-01/ex04/Transfer.cpp
+++ Module-01/ex04/Transfer.cpp
@@ -6,7 +6,7 @@
 /*   By: mel-hamd <mel-hamd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 10:46:46 by mel-hamd          #+#    #+#             */
-/*   Updated: 2024/12/21 16:59:44 by mel-hamd         ###   ########.fr       */
+/*   Updated: 2024/12/24 14:08:54 by mel-hamd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,21 +24,21 @@ Transfer::Transfer(std::string inputFile, std::string s1,  std::string s2)
 
 void    Transfer::transferData()
 {
-    std::ifstream inFile(this->inputFile);
-    if (!inFile)
+    std::ifstream inFile(this->inputFile.c_str());
+    std::ofstream outFile((this->inputFile + ".replace").c_str());
+	if (!inFile)
     {   
         std::cout << "Cant open file : " << this->inputFile << std::endl;
         return ;
     }
-    std::ofstream outFile(this->inputFile + ".replace");
 	
-    if (!outFile)
+	if (!outFile)
     {   
         std::cout << "Cant open file : " << this->inputFile + ".replace" << std::endl;
         return ;
     }
 
-    std::string line = "";
+    std::string line;
 
     while (!std::getline(inFile, line).eof())
     {
@@ -56,6 +56,4 @@ void    Transfer::transferData()
         res += line.substr(pos);
         outFile << res << std::endl;
     }
-    inFile.close();
-    outFile.close();
 }
