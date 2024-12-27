a4f1a2135133e306d80536aeb9e3f8c2a180b776
Mohammed EL Hamdaoui <melhamdaoui811@gmail.com>
2024-12-27 19:07:50 +0100

    Module-02/ex00 : implements the class

--- Module-02/ex00/Fixed.cpp
+++ Module-02/ex00/Fixed.cpp
@@ -6,7 +6,39 @@
 /*   By: mel-hamd <mel-hamd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 10:34:23 by mel-hamd          #+#    #+#             */
-/*   Updated: 2024/12/26 10:34:24 by mel-hamd         ###   ########.fr       */
+/*   Updated: 2024/12/27 19:07:20 by mel-hamd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+const int Fixed::fractBites = 0;
+
+int Fixed::getFixedValue() const
+{
+	return this->fixedValue;
+}
+
+Fixed::Fixed() : fixedValue(0)
+{
+	std::cout << "Default constractor called !" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &copy) : fixedValue(copy.getFixedValue())
+{
+	std::cout << "copy contructor called !" << std::endl;
+}
+
+Fixed &Fixed::operator=(const Fixed &copy)
+{
+	if (this == &copy)
+		return (*this);
+	this->fixedValue = copy.getFixedValue();
+	std::cout << "Copy assignment operator called !";
+	return (*this);
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called !" << std::endl;
+}
--- Module-02/ex00/Fixed.hpp
+++ Module-02/ex00/Fixed.hpp
@@ -6,21 +6,27 @@
 /*   By: mel-hamd <mel-hamd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 10:34:36 by mel-hamd          #+#    #+#             */
-/*   Updated: 2024/12/26 11:59:59 by mel-hamd         ###   ########.fr       */
+/*   Updated: 2024/12/27 18:00:00 by mel-hamd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 #define FIXED_HPP
 
-class Fixed {
-	private :
-			int fixedValue;
-			static const int fractBites;
-	public :
-		Fixed();
-		Fixed(const Fixed &copy);
-};
+#include <iostream>
 
+class Fixed
+{
+private:
+	int fixedValue;
+	static const int fractBites;
+
+public:
+	Fixed();
+	Fixed(const Fixed &copy);
+	Fixed &operator=(const Fixed &copy);
+	~Fixed();
+	int getFixedValue() const;
+};
 
 #endif
