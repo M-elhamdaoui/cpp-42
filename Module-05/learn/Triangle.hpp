a6fe87d60416eedb26821e7a6c60bb48e2702b00
M-elhamdaoui <melhamdaoui811@gmail.com>
2025-04-19 10:07:33 +0100

    Module-05 : learn : setup Triangle hpp file

---
+++ Module-05/learn/Triangle.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Triangle.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-hamd <mel-hamd@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/19 08:15:30 by mel-hamd          #+#    #+#             */
+/*   Updated: 2025/04/19 09:51:45 by mel-hamd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <array>
+
+class Triangle {
+	private :
+	std::array<int, 2> a;
+	std::array<int, 2> b;
+	std::array<int, 2> c;
+	public :
+		Triangle();
+		Triangle(const Triangle& copy);
+		Triangle& operator= (const Triangle& copy);
+		~Triangle();
+
+		std::array<int, 2> getA() const;
+		std::array<int, 2> getB() const;
+		std::array<int, 2> getC() const;
+
+		void setA(int x, int y);
+		void setB(int x, int y);
+		void setC(int x, int y);
+
+};
