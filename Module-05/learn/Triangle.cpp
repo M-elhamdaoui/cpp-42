77259f7e3da6d68115c3db23d5a7604a83e65fc6
M-elhamdaoui <melhamdaoui811@gmail.com>
2025-04-19 10:37:59 +0100

    Module-05 : learn : creat triangle class

--- Module-05/learn/Triangle.cpp
+++ Module-05/learn/Triangle.cpp
@@ -6,9 +6,60 @@
 /*   By: mel-hamd <mel-hamd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 10:04:11 by mel-hamd          #+#    #+#             */
-/*   Updated: 2025/04/19 10:06:50 by mel-hamd         ###   ########.fr       */
+/*   Updated: 2025/04/19 10:31:26 by mel-hamd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Triangle.hpp"
 
+Triangle::Triangle() {
+	this->a[0] = 0;
+	this->a[1] = 0;
+	this->b[0] = 0;
+	this->b[1] = 0;
+	this->c[0] = 0;
+	this->c[1] = 0;
+
+}
+
+Triangle::Triangle(const Triangle& copy) {
+	*this = copy;
+}
+
+Triangle& Triangle::operator= (const Triangle& copy) {
+	if (this != &copy) {
+		this->a = copy.a;
+		this->b =  copy.b;
+		this->c = copy.c;
+	}
+	return (*this);
+}
+
+Triangle::~Triangle() {
+	std::cout << "Destructor !" << std::endl;
+}
+
+std::array<int, 2> Triangle::getA() const {
+	return this->a;
+}
+std::array<int, 2> Triangle::getB() const {
+	return this->b;
+}
+std::array<int, 2> Triangle::getC() const {
+	return this->c;
+}
+
+void Triangle::setA(int x, int y) {
+	this->a[0] = x;
+	this->a[1] = y;
+}
+
+void Triangle::setB(int x,  int y) {
+	this->b[0] = x;
+	this->b[1] = y;
+}
+
+void Triangle::setC(int x, int y) {
+	this->c[0] = x;
+	this->c[1] = y;
+}
--- Module-05/learn/main.cpp
+++ Module-05/learn/main.cpp
@@ -6,21 +6,25 @@
 /*   By: mel-hamd <mel-hamd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 07:00:18 by mel-hamd          #+#    #+#             */
-/*   Updated: 2025/04/19 08:06:26 by mel-hamd         ###   ########.fr       */
+/*   Updated: 2025/04/19 10:27:41 by mel-hamd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "Rectangle.hpp"
-
+# include "Triangle.hpp"
 
 int  main() {
 	try
 	{
-		Rectangle rect;
+		Rectangle	rect;
+		Triangle	tri;
 
 		rect.setHeight(50);
 		rect.setWidth(-50);
 
+		tri.setA(10, 20);
+		tri.setB(20, 5);
+		tri.setC(30, 2);
 		std::cout << "Height : " << rect.getHeight() << " | Width : " << rect.getWidth() << std::endl;
 	}
 	catch(const std::invalid_argument& e)
