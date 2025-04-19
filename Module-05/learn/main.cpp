5158dd4d4e041faccc2538e7941654b36bbce10f
M-elhamdaoui <melhamdaoui811@gmail.com>
2025-04-19 08:07:43 +0100

    Module-05: learn : Catch the error with try-catch block now the programe works well witout errors

--- Module-05/learn/main.cpp
+++ Module-05/learn/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mel-hamd <mel-hamd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 07:00:18 by mel-hamd          #+#    #+#             */
-/*   Updated: 2025/04/19 07:11:11 by mel-hamd         ###   ########.fr       */
+/*   Updated: 2025/04/19 08:06:26 by mel-hamd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,19 @@
 
 
 int  main() {
-	Rectangle rect;
+	try
+	{
+		Rectangle rect;
 
-	rect.setHeight(50);
-	rect.setWidth(-50);
+		rect.setHeight(50);
+		rect.setWidth(-50);
 
-	std::cout << "Height : " << rect.getHeight() << " | Width : " << rect.getWidth() << std::endl;
+		std::cout << "Height : " << rect.getHeight() << " | Width : " << rect.getWidth() << std::endl;
+	}
+	catch(const std::invalid_argument& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
 	return (0); 
 }
