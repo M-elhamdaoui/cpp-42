4907ef911788f8fc73da020985ff70f05f4b854b
M-elhamdaoui <melhamdaoui811@gmail.com>
2025-04-22 16:57:23 +0100

    Module-05: ex01 : Setup Form with exceptions

--- Module-05/ex01/Form.hpp
+++ Module-05/ex01/Form.hpp
@@ -6,13 +6,14 @@
 /*   By: mel-hamd <mel-hamd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 16:04:24 by mel-hamd          #+#    #+#             */
-/*   Updated: 2025/04/22 16:15:12 by mel-hamd         ###   ########.fr       */
+/*   Updated: 2025/04/22 16:56:40 by mel-hamd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
+#include "Bureaucrat.hpp"
 
 class Form {
 	private :
@@ -23,10 +24,45 @@ class Form {
 
 	public :
 		Form();
+		Form(int);
 		Form(const Form& copy);
 		~Form();
 		
 		Form& operator=(const Form& copy);
 
-	
+		void  beSigned();
+
+		std::string getName() const;
+		int getRequiredGradeToSign();
+		int getRequiredGradeToExec();
+
+		class GradeTooLowException : public std::exception {
+			private :
+				const std::string msg;
+			public :
+				GradeTooLowException(std::string msg) : msg(msg)
+				{
+
+				}
+				const char* what() const throw() {
+					return (this->msg.c_str());
+				}
+				~GradeTooLowException() throw() {
+					
+				}
+		};
+		class GradeTooHighException : public std::exception {
+			private :
+					const std::string msg;
+			public :
+			 		GradeTooHighException(std::string msg) : msg(msg){
+
+					}
+					const char* what() const throw() {
+						return (this->msg.c_str());	
+					}
+					~GradeTooHighException() throw() {
+
+					}
+		};
 };
