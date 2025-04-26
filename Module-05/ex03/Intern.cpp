d62e8be4e1839228b739a6d4335222f1bd0d34b4
M-elhamdaoui <melhamdaoui811@gmail.com>
2025-04-26 09:40:18 +0100

    Module-05 ex03  finished

--- Module-05/ex03/Intern.cpp
+++ Module-05/ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: mel-hamd <mel-hamd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 06:10:21 by mel-hamd          #+#    #+#             */
-/*   Updated: 2025/04/26 08:40:49 by mel-hamd         ###   ########.fr       */
+/*   Updated: 2025/04/26 09:17:20 by mel-hamd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ Intern::Intern () {
 }
 
 Intern::Intern(const Intern& copy) {
-    if (copy)
+    if (&copy != this)
         return ;
 }
 
@@ -25,7 +25,7 @@ Intern::~Intern() {
 
 }
 
-Intern& operator=(const Intern& copy) {
+Intern& Intern::operator=(const Intern& copy) {
     if (this != &copy)
         return (*this);
     return (*this);
@@ -34,21 +34,28 @@ Intern& operator=(const Intern& copy) {
 AForm* Intern::makeForm(const std::string& name, const std::string& target) {
     std::string names[3] = {"presidential pardon", "robotomy request", "shrubbery creation"};
     AForm* forms[3] = {new PresidentialPardonForm(target), new RobotomyRequestForm(target), new ShrubberyCreationForm(target)};
-    
-    for (int i = 0 ; i < 3 ; i++)
+    int i = 0;
+
+    while (i < 3)
     {
-        if (name == name[i]) {
+        if (name == names[i]) {
             break;
         }
+        i++;
     }
     if (i < 3)
     {
         for (int j = 0; j < 3; j++)
         {
             if (j != i)
-                delete forms[j]
+                delete forms[j];
         }
+        std::cout << "Intern creates " << forms[i]->getName() << std::endl;
         return forms[i];
     }
+    for (int j = 0; j < 3; j++) {
+        delete forms[j];
+    }
+    std::cout << "Intern can't create " <<  name << " form" << std::endl;
     return (NULL);
 }
