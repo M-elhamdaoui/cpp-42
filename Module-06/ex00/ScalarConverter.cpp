d1038378926f3afe1fe52ff0253e5f9518769387
Mohammed EL Hamdaoui <melhamdaoui811@gmail.com>
2025-04-27 16:46:13 +0100

    Module-06 ex00 print all about char

--- Module-06/ex00/ScalarConverter.cpp
+++ Module-06/ex00/ScalarConverter.cpp
@@ -5,7 +5,7 @@ ScalarConverter::ScalarConverter() {
 }
 
 ScalarConverter::ScalarConverter( const ScalarConverter& copy) {
-
+        *this = copy;
 }
 
 ScalarConverter::~ScalarConverter() {
@@ -13,12 +13,30 @@ ScalarConverter::~ScalarConverter() {
 }
 
 ScalarConverter& ScalarConverter::operator=(const ScalarConverter& copy) {
-
+    if(this != &copy)
+        return *this;
+    return *this;
 }
 
 void ScalarConverter::convert(const std::string& str) {
-    std::cout << "char: " << "char here " << std::endl;
-    std::cout << "int: " << "int here " << std::endl;
-    std::cout << "float: " << "float here " << std::endl;
-    std::cout << "double: " << "double here " << std::endl;
+    char *rest;
+    double res = std::strtod(str.c_str(), &rest);
+
+    if (str.length() == 1) {
+        if (str[0] < 0 || str[0] > 127) 
+            std::cout << "char: " << "impossible" << std::endl;
+        else if (str[0] >= 32 && str[0] < 127)
+            std::cout << "char: " << str << std::endl;
+        else
+            std::cout << "char: " << "Non displayable" << std::endl;
+
+        // std::cout << "int: " << "int here " << std::endl;
+        // std::cout << "float: " << "float here " << std::endl;
+        // std::cout << "double: " << "double here " << std::endl;
+    }
+    // std::cout << "char: " << "char here " << std::endl;
+    // std::cout << "int: " << "int here " << std::endl;
+    // std::cout << "float: " << "float here " << std::endl;
+    // std::cout << "double: " << "double here " << std::endl;
+    std::cout << res << std::endl;
 }
