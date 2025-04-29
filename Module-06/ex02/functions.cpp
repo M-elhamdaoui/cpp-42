c89c6a4c6db47e4b09bb083badf65b8610a1cf3a
M-elhamdaoui <melhamdaoui811@gmail.com>
2025-04-29 13:20:58 +0100

    Module-06 ex02 finished

---
+++ Module-06/ex02/functions.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   functions.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-hamd <mel-hamd@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/29 13:07:55 by mel-hamd          #+#    #+#             */
+/*   Updated: 2025/04/29 13:14:39 by mel-hamd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "functions.hpp"
+
+
+Base * generate(void) {
+	size_t num;
+	std::srand(std::time(0));
+	num = rand();
+	if (num % 3 == 0)
+		return new A();
+	else if (num % 3 == 1)
+		return new B();
+	else
+		return new C();
+}
+
+void identify(Base* p) {
+	if (dynamic_cast<A*>(p))
+		std::cout << "its Type A " << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "its Type B " << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "its Type C " << std::endl;
+	else
+		std::cout << "Unknown type " << std::endl;
+}
---
+++ Module-06/ex02/functions.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   functions.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-hamd <mel-hamd@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/29 13:03:41 by mel-hamd          #+#    #+#             */
+/*   Updated: 2025/04/29 13:19:15 by mel-hamd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <cstdlib>
+
+Base * generate(void);
+void identify(Base* p);
