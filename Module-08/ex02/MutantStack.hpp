1aee0a119b165bf3cdf5fd3bdd3408e38e5e543c
Mohammed EL Hamdaoui <melhamdaoui811@gmail.com>
2025-05-09 06:14:03 +0100

    Module-08 ex02 : setup

---
+++ Module-08/ex02/MutantStack.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-hamd <mel-hamd@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/09 05:55:33 by mel-hamd          #+#    #+#             */
+/*   Updated: 2025/05/09 06:13:16 by mel-hamd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <list>
+
+class MutantStack  {
+    private :
+
+    public :
+    
+};
