4c8c52b6251eab16f9c32e52bde0360901c5d001
Mohammed EL Hamdaoui <melhamdaoui811@gmail.com>
2025-05-12 09:44:55 +0100

    Modue-09 ex01 finished

--- Module-09/ex01/RPN.cpp
+++ Module-09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: mel-hamd <mel-hamd@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 06:43:57 by mel-hamd          #+#    #+#             */
-/*   Updated: 2025/05/12 09:32:13 by mel-hamd         ###   ########.fr       */
+/*   Updated: 2025/05/12 09:44:06 by mel-hamd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,41 +34,43 @@ RPN& RPN::operator=(const RPN& copy) {
 void RPN::processRPN(const char* str) {
     std::stringstream ss(str);
     std::string token;
-    std::stack<long> st;
+    std::stack<double> st;
     while (std::getline(ss, token, ' ')) {
+        if (token.size() == 0)
+            continue;
         if (token.size() != 1)
             throw std::logic_error("Error");
         if (token[0] >= '0' && token[0] <= '9')
             st.push(std::atoi(token.c_str()));
         else if (token == "+" && st.size() > 1)
         {
-            long second = st.top();
+            double second = st.top();
             st.pop();
-            long first = st.top();
+            double first = st.top();
             st.pop();
             st.push(first + second);
         }
         else if (token == "-" && st.size() > 1)
         {
-            long second = st.top();
+            double second = st.top();
             st.pop();
-            long first = st.top();
+            double first = st.top();
             st.pop();
             st.push(first - second);
         }
         else if (token == "*" && st.size() > 1)
         {
-            long second = st.top();
+            double second = st.top();
             st.pop();
-            long first = st.top();
+            double first = st.top();
             st.pop();
             st.push(first * second);
         }
         else if (token == "/" && st.size() > 1)
         {
-            long second = st.top();
+            double second = st.top();
             st.pop();
-            long first = st.top();
+            double first = st.top();
             if (second == 0)
                 throw std::logic_error("ERROR");
             st.pop();
